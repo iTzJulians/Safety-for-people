9dc2c745aa37daba735859541411929caf64e504
Martha <caromartha175@gmail.com>
2026-08-21 20:33:30 -0500

    feat: agregar funcion para eliminar productos del carrito

--- pages/productos/carrito.js
+++ pages/productos/carrito.js
@@ -5,7 +5,7 @@ let carrito = [
     descripcion: "descripcion del producto, características breves",
     precio: 200900,
     cantidad: 1,
-    imagen: "reloj.png"
+    imagen: "reloj.png",
   },
   {
     id: 2,
@@ -13,48 +13,51 @@ let carrito = [
     descripcion: "descripcion del producto, características breves",
     precio: 200900,
     cantidad: 1,
-    imagen: "reloj.png"
-  }
+    imagen: "reloj.png",
+  },
 ];
 
+function actualizarCarrito() {
+  carrito.forEach((producto) => {
+    const contador = document.getElementById(`cantidad-${producto.id}`);
 
-function actualizarCarrito(){
-
-    carrito.forEach(producto =>{
-
-        const contador = document.getElementById(`cantidad-${producto.id}`)
-
-    
-
-        if (contador){
-            contador.textContent = producto.cantidad;
-        }
-    });
+    if (contador) {
+      contador.textContent = producto.cantidad;
+    }
+  });
 }
 
- function aumentarCantidad(id){
-    const producto = carrito.find(producto => producto.id === id);
-    
-    if (producto){
-        producto.cantidad++;
-        actualizarCarrito();
-    }
- }
+function aumentarCantidad(id) {
+  const producto = carrito.find((producto) => producto.id === id);
+
+  if (producto) {
+    producto.cantidad++;
+    actualizarCarrito();
+  }
+}
 
 function disminuirCantidad(id) {
-    const producto = carrito.find(producto => producto.id === id);
+  const producto = carrito.find((producto) => producto.id === id);
 
-    if (producto) {
-        producto.cantidad--;
+  if (producto) {
+    producto.cantidad--;
 
-         if (producto.cantidad === 0) {
-            carrito = carrito.filter(item => item.id !== id);
-            console.josn?.("Objeto eliminado del carrito");  
-        }
+    if (producto.cantidad === 0) {
+      carrito = carrito.filter((item) => item.id !== id);
+      console.josn?.("Objeto eliminado del carrito");
+    }
 
-        actualizarCarrito();
-        console.log("Carrito actual:", carrito);
-    };
-};
+    actualizarCarrito();
+    console.log("Carrito actual:", carrito);
+  }
+}
 
+// Función para eliminar un producto del carrito
+function eliminarProducto(id) {
+  carrito = carrito.filter((producto) => producto.id !== id);
 
+  actualizarCarrito();
+
+  console.log("Producto eliminado del carrito");
+  console.log("Carrito actual:", carrito);
+}
